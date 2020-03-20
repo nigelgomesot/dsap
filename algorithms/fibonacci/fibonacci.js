e7757b5b9c1701ff34caa1bfe81c6249c88a1188
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-03-20 07:38:34 -0500

    # list added...

--- algorithms/fibonacci/fibonacci.js
+++ algorithms/fibonacci/fibonacci.js
@@ -1,3 +1,29 @@
 // REF: https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/fibonacci
 
+export function list(number) {
+	const result = [1];
 
+	if (number === 1) {
+		return result;
+	}
+
+	result.push(1);
+
+	if (number === 2) {
+		return result;
+	}
+
+	let counter = number - 2;
+
+	while (counter) {
+		const preceedingValue1 = result[result.length - 1];
+		const preceedingValue2 = result[result.length - 2];
+		const currentValue = preceedingValue1 + preceedingValue2;
+
+		result.push(currentValue);
+
+		counter -= 1;
+	}
+
+	return result;
+}
--- algorithms/fibonacci/fibonacci.test.js
+++ algorithms/fibonacci/fibonacci.test.js
@@ -1,1 +1,11 @@
 import * as Fibonacci from './fibonacci';
+
+describe('Fibonacci', () => {
+
+	it('returns list of fibonacci numbers', () => {
+		expect(Fibonacci.list(1)).toEqual([1]);
+		expect(Fibonacci.list(2)).toEqual([1,1]);
+		expect(Fibonacci.list(6)).toEqual([1,1,2,3,5,8]);
+		expect(Fibonacci.list(8)).toEqual([1,1,2,3,5,8,13,21]);
+	})
+});
