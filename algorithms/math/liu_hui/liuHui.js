5ed87b0d92d4daf64ec0422caa7f93d215cd0ff0
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-04-14 08:24:45 -0500

    # liuHui WIP...

--- algorithms/math/liu_hui/liuHui.js
+++ algorithms/math/liu_hui/liuHui.js
@@ -1,5 +1,18 @@
 // REF: https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/liu-hui
 
+function getNGonSideLength(sideLength, splitCounter) {
+	if (splitCounter <= 0) {
+		return sideLength;
+	}
+
+	const halfSide = sideLength / 2;
+	const perpendicular = Math.sqrt((circleRadius ** 2) - (halfSide ** 2));
+	const excessRadius = circleRadius - perpendicular;
+	const splitSideLength = Math.sqrt((excessRadius ** 2) + (halfSide ** 2));
+
+	return getNGonSideLength(splitSideLength, splitCounter - 1);
+}
+
 export default function liuHui(splitCount) {
 
 }
