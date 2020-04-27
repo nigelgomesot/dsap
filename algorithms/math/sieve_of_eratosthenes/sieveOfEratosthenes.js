8dab7486e4d66625b2ee71d16649665a921e6e86
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-04-27 08:20:46 -0500

    # sieveOfEratosthenes fetch added...

--- algorithms/math/sieve_of_eratosthenes/sieveOfEratosthenes.js
+++ algorithms/math/sieve_of_eratosthenes/sieveOfEratosthenes.js
@@ -1,5 +1,24 @@
 // REF: https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/sieve-of-eratosthenes
 
-export function fetch(limit) {
+export function fetch(maxNumber) {
+	const isPrime = new Array(maxNumber + 1).fill(true);
+	const primes = [];
 
+	isPrime[0] = false;
+	isPrime[1] = false;
+
+	for(let number = 2; number <= maxNumber; number++) {
+		if (isPrime[number]) {
+			primes.push(number);
+		}
+
+		let nextNumber = number * number;
+
+		while(nextNumber <= maxNumber) {
+			isPrime[nextNumber] = false;
+			nextNumber += number;
+		}
+	}
+
+	return primes;
 }
