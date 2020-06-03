cc69eb6bd723a72c78fbe74d08194dee2243dd11
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-06-03 09:02:06 -0500

    getLPSLength added...

--- algorithms/dynamic_programming/17_longest_palindrome_subsequence.js
+++ algorithms/dynamic_programming/17_longest_palindrome_subsequence.js
@@ -2,8 +2,31 @@
 
 export function getLPSLength(str) {
   const strLength = str.length;
-  const matrix = new Array(strLength + 1).fill(null).map(() => Array(strLength + 1));
+  const matrix = new Array(strLength).fill(null).map(() => Array(strLength));
 
-  // PENDING:
+  for (let i = 0; i < strLength; i++) {
+    matrix[i][i] = 1;
+  }
+
+  for (let length = 2; length <= strLength; length++) {
+    for (let row = 0; row < strLength - length + 1; row++) {
+
+      const col = row + length - 1;
+
+      if ((length === 2) && (str[row] === str[col])) {
+        matrix[row][col] == 2;
+      } else if (str[row] === str[col]) {
+        matrix[row][col] = 2 + matrix[row + 1][col - 1];
+      } else {
+        matrix[row][col] =  Math.max(
+          matrix[row][col - 1],
+          matrix[row + 1][col]
+        );
+      }
+    }
+  }
+
   console.log(matrix);
+
+  return matrix[0][strLength - 1];
 }
