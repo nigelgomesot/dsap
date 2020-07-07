daa4d90687c436f7208d54f6161f3d323871ef87
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-07-07 10:16:43 -0500

    # WIP...

--- data_structures/techiedelight/src/03_dfs_travel_time.test.js
+++ data_structures/techiedelight/src/03_dfs_travel_time.test.js
@@ -2,8 +2,46 @@
 
 import Graph from './graph.js';
 
-function dfsTravelTime(graph) {
+function dfsTravelTime(graph, startIndex) {
+  const visited = Array(graph.size).fill(false);
+  const arrivalTime = Array(graph.size).fill(-1);
+  const departureTime = Array(graph.size).fill(-1);
+  const traversed = new Array();
+  const stack = new Array();
+  let time = -1;
 
+
+  stack.push(startIndex);
+
+  while (stack.length > 0) {
+    const nodeIndex = stack.pop();
+
+    if (visited[nodeIndex]) {
+      continue;
+    }
+    visited[nodeIndex] = true;
+    arrivalTime[nodeIndex] = time++;
+    traversed.push(nodeIndex);
+
+    let head = graph.list[nodeIndex].head;
+
+    while (head) {
+      if (!visited[head.destination]) {
+        stack.push(head.destination);
+        arrivalTime[head.destination] = time++;
+      }
+
+      head = head.next;
+    }
+
+    departureTime[nodeIndex] = time++;
+  }
+
+  return {
+    arrivalTime: arrivalTime,
+    departureTime: departureTime,
+    traversed: traversed,
+  };
 }
 
 describe('DFS Travel Time', () => {
@@ -25,9 +63,9 @@ describe('DFS Travel Time', () => {
     ]
     graph = new Graph(elementCount);
     graph.addEdges(edges);
-    const travelTimes = dfsTravelTime(graph);
-
-    expect(travelTimes['arrival']).toEqual([0, 1, 3, 4, 8, 5, 12, 13]);
-    expect(travelTimes['departure']).toEqual([11, 2, 10, 7, 9, 6, 15, 14]);
+    const travelTimes = dfsTravelTime(graph, 0);
+    console.log('traversed', travelTimes['traversed']);
+    expect(travelTimes['arrivalTime']).toEqual([0, 1, 3, 4, 8, 5, 12, 13]);
+    expect(travelTimes['departureTime']).toEqual([11, 2, 10, 7, 9, 6, 15, 14]);
   });
 });
