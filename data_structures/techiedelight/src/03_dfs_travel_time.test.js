a3e2ff97f35b9cef654af36b6ba69ed309dbdf80
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-07-11 10:41:09 -0500

    # dfsTravelTime added...

--- data_structures/techiedelight/src/03_dfs_travel_time.test.js
+++ data_structures/techiedelight/src/03_dfs_travel_time.test.js
@@ -3,57 +3,51 @@
 
 import Graph from './graph.js';
 
-function dfsTravelTimeIterative(graph, startIndex) {
-  const visited = Array(graph.size).fill(false);
-  const arrivalTime = Array(graph.size).fill(-1);
-  const departureTime = Array(graph.size).fill(-1);
-  const traversed = new Array();
-  const stack = new Array();
-  let time = 0;
-
-  arrivalTime[startIndex] = time;
-  stack.push(startIndex);
-
-  while (stack.length > 0) {
-    const nodeIndex = stack.pop();
-
-    if (visited[nodeIndex]) {
-      continue;
-    }
-    visited[nodeIndex] = true;
-    //departureTime[nodeIndex] = ++time;
-    traversed.push(nodeIndex);
-
-    let head = graph.list[nodeIndex].head;
-
-    while (head) {
-      if (!visited[head.destination]) {
-        arrivalTime[head.destination] = ++time;
-        stack.push(head.destination);
-      }
-
-      head = head.next;
-    }
-  }
-
-  return {
-    arrivalTime: arrivalTime,
-    departureTime: departureTime,
-    traversed: traversed,
-  };
-}
-
 function dfsTravelTime(graph, startIndex) {
   const visited = Array(graph.size).fill(false);
   const arrivalTime = Array(graph.size).fill(-1);
   const departureTime = Array(graph.size).fill(-1);
-  let time = -1;
+  let time = 0;
 
-  dfsTravelTimeRecursive(graph, startIndex, visited, arrivalTime, departureTime, time);
+  dfsTravelTimeRecursive(graph,
+    startIndex,
+    visited,
+    arrivalTime,
+    departureTime,
+    time);
+
+  return {
+    arrivalTime: arrivalTime,
+    departureTime: departureTime,
+  };
 }
 
-function dfsTravelTimeRecursive(graph, nodeIndex, visited, arrivalTime, departureTime, time) {
-  // PENDING
+function dfsTravelTimeRecursive(graph,
+  nodeIndex,
+  visited,
+  arrivalTime,
+  departureTime,
+  time) {
+  visited[nodeIndex] = true;
+  arrivalTime[nodeIndex] = time++;
+
+  let head = graph.list[nodeIndex].head;
+
+  while (head) {
+    if (!visited[head.destination]) {
+      time = dfsTravelTimeRecursive(graph,
+        head.destination,
+        visited,
+        arrivalTime,
+        departureTime,
+        time);
+    }
+    head = head.next;
+  }
+
+  departureTime[nodeIndex] = time++;
+
+  return time;
 }
 
 describe('DFS Travel Time', () => {
@@ -70,13 +64,28 @@ describe('DFS Travel Time', () => {
       {source: 2, destination: 3},
       {source: 3, destination: 4},
     ]
-    graph = new Graph(elementCount, true);
+    graph = new Graph(elementCount);
     graph.addEdges(edges);
     travelTimes = dfsTravelTime(graph, 0);
     console.dir('travelTimes', travelTimes);
     expect(travelTimes['arrivalTime']).toEqual([0, 1, 2, 3, 4]);
     expect(travelTimes['departureTime']).toEqual([9, 8, 7, 6, 5]);
-    return;
+
+    elementCount = 7;
+    edges = [
+      {source: 1, destination: 2},
+      {source: 1, destination: 3},
+      {source: 2, destination: 4},
+      {source: 2, destination: 5},
+      {source: 3, destination: 6},
+    ]
+    graph = new Graph(elementCount);
+    graph.addEdges(edges);
+    travelTimes = dfsTravelTime(graph, 1);
+    console.dir('travelTimes', travelTimes);
+    expect(travelTimes['arrivalTime']).toEqual([-1, 0, 1, 7, 2, 4, 8]);
+    expect(travelTimes['departureTime']).toEqual([-1, 11, 6, 10, 3, 5, 9]);
+
     elementCount = 8;
     edges = [
       {source: 0, destination: 1},
@@ -88,11 +97,11 @@ describe('DFS Travel Time', () => {
       {source: 4, destination: 5},
       {source: 6, destination: 7},
     ]
-    graph = new Graph(elementCount, true);
+    graph = new Graph(elementCount);
     graph.addEdges(edges);
     travelTimes = dfsTravelTime(graph, 0);
     console.dir('travelTimes', travelTimes);
-    expect(travelTimes['arrivalTime']).toEqual([0, 1, 3, 4, 8, 5, 12, 13]);
-    expect(travelTimes['departureTime']).toEqual([11, 2, 10, 7, 9, 6, 15, 14]);
+    expect(travelTimes['arrivalTime']).toEqual([0, 1, 3, 4, 8, 5, -1, -1]);
+    expect(travelTimes['departureTime']).toEqual([11, 2, 10, 7, 9, 6, -1, -1]);
   });
 });
