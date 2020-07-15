4a96ff96a9ac5dd574958e5451901abc3e0f06c2
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-07-15 07:49:26 -0500

    # dfs(biGraph) WIP...

--- data_structures/techiedelight/src/04_bipartite.test.js
+++ data_structures/techiedelight/src/04_bipartite.test.js
@@ -3,7 +3,7 @@
 import Graph from './graph.js';
 import Queue from './queue.js';
 
-function isBiGraphViaBFS(graph, startIndex) {
+function bfs(graph, startIndex) {
   const visited = new Array(graph.size).fill(false);
   const level = new Array(graph.size).fill(-1);
   const queue = new Queue();
@@ -34,8 +34,44 @@ function isBiGraphViaBFS(graph, startIndex) {
   return true;
 }
 
+function dfs(graph, startIndex) {
+  const visited = new Array(graph.size).fill(-1);
+  const colored = new Array(graph.size).fill(null);
+  visited[startIndex] = true;
+  colored[startIndex] = true;
+
+  const isBiGraph = dfsRecursive(graph, startIndex, visited, colored);
+
+  return isBiGraph;
+}
+
+function dfsRecursive(graph, nodeIndex, visited, colored) {
+  let head = graph.list[nodeIndex].head;
+
+  while (head) {
+    if (!visited[head.destination]) {
+      visited[head.destination] = true;
+      colored[head.destination] = !colored[head.source];
+
+      const isBiGraph = dfsRecursive(graph, head.destination, visited, colored);
+
+      if (!isBiGraph) {
+        return false;
+      }
+    }
+  
+    if (colored[head.source] === colored[head.destination]) {
+      return false;
+    }
+
+    head = head.next;
+  }
+
+  return true;
+}
+
 describe('Bipartite Graph', () => {
-  it('verifies bigraph via BFS', () => {
+  it('verifies is bi-graph via BFS', () => {
     let elementCount;
     let edges;
     let graph;
@@ -53,11 +89,37 @@ describe('Bipartite Graph', () => {
     ];
     graph = new Graph(elementCount);
     graph.addEdges(edges);
-    expect(isBiGraphViaBFS(graph, 1)).toBe(true);
+    expect(bfs(graph, 1)).toBe(true);
 
     edges.push({source: 2, destination: 4})
     graph = new Graph(elementCount);
     graph.addEdges(edges);
-    expect(isBiGraphViaBFS(graph, 1)).toBe(false);
+    expect(bfs(graph, 1)).toBe(false);
+  });
+
+  it('verifies is bi-graph via DFS', () => {
+    let elementCount;
+    let edges;
+    let graph;
+
+    elementCount = 10;
+    edges = [
+      {source: 1, destination: 2},
+      {source: 2, destination: 3},
+      {source: 2, destination: 8},
+      {source: 3, destination: 4},
+      {source: 4, destination: 6},
+      {source: 5, destination: 7},
+      {source: 5, destination: 9},
+      {source: 8, destination: 9},
+    ];
+    graph = new Graph(elementCount);
+    graph.addEdges(edges);
+    expect(dfs(graph, 1)).toBe(true);
+
+    // edges.push({source: 2, destination: 4})
+    // graph = new Graph(elementCount);
+    // graph.addEdges(edges);
+    // expect(dfs(graph, 1)).toBe(false);
   });
 });
