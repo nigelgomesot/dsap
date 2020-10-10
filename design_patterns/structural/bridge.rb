9eabf6342853af320bc054276d6dff37bb91a220
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-10-10 10:30:02 -0500

    # bridge added...

--- design_patterns/structural/bridge.rb
+++ design_patterns/structural/bridge.rb
@@ -3,6 +3,9 @@
 # Problem: Split a class having multiple dimensions: abstraction & implementation.
 # Solution: Extract one dimension(implementation) into a separate class & reference it from another class(abstraction).
 
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
 class Abstraction
   def initialize(implementation)
     @implementation = implementation
@@ -41,5 +44,12 @@ def client(abstraction)
   abstraction.operation
 end
 
-# PENDING:
+implementation_a = ConcreteImplementationA.new
+abstraction_a = Abstraction.new(implementation_a)
 
+implementation_b = ConcreteImplementationB.new
+abstraction_b = ExtendedAbstraction.new(implementation_b)
+
+assert_equal client(abstraction_a), 'base abstraction operation: concrete implementation A'
+assert_equal client(abstraction_b), 'extended abstraction operation: concrete implementation B'
+puts('done.')
