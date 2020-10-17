a3c174cc967738c69d4490d590007392cad0b1e3
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-10-17 09:42:10 -0500

    # facade added...

--- design_patterns/structural/facade.rb
+++ design_patterns/structural/facade.rb
@@ -3,6 +3,9 @@
 # Problem: provide a simplified & limited interface to a complex system
 # Solution: Implement a new class which has limited interface & redirects client calls to approproate objects in subsystem
 
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
 class Facade
   def initialize(subsystem1, subsystem2)
     @subsystem1 = subsystem1
@@ -33,7 +36,7 @@ class Subsystem1
 end
 
 class Subsystem2
-  def operation_2
+  def operation_1
     'subsystem 2 operation 1'
   end
 
@@ -46,4 +49,19 @@ def client(facade)
   facade.operation
 end
 
-# PENDING
+subsystem1 = Subsystem1.new()
+subsystem2 = Subsystem2.new()
+
+facade = Facade.new(subsystem1, subsystem2)
+
+expected_results = [
+  'initialize facade',
+  'subsystem 1 operation 1',
+  'subsystem 2 operation 1',
+  'perform operations',
+  'subsystem 1 operation 2',
+  'subsystem 2 operation 2',
+]
+
+assert_equal client(facade), expected_results
+puts('done')
