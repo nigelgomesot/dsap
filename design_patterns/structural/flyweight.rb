42e707b11fb4cf9ebf21db987d048f8dc432bd34
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-10-22 09:41:50 -0500

    # flyweight added...

--- design_patterns/structural/flyweight.rb
+++ design_patterns/structural/flyweight.rb
@@ -3,13 +3,16 @@
 # Problem: allow programs to suppport vast quantities of objects by keeping their memory consumption low
 # Solution: Share parts of objects between multiple objects(intrinsic data), & expose methods to compute extrinsic data.
 
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
 class Flyweight
   def initialize(shared_state)
     @shared_state = shared_state
   end
 
-  def operation
-    {shared: @shared_state, unique: @unique_state}
+  def operation(unique_state)
+    {shared: @shared_state, unique: unique_state}
   end
 end
 
@@ -36,7 +39,7 @@ class FlyweightFactory
   end
 
   def list_flyweights
-    @flyweights.keys.join(',')
+    @flyweights.keys
   end
 end
 
@@ -46,6 +49,25 @@ def client_add_car_to_db(factory, plates, owner, brand, model, color)
   flyweight.operation([plates, owner])
 end
 
+factory = FlyweightFactory.new([
+  %w[Chevrolet Camaro2018 pink],
+  ['Mercedes Benz', 'C300', 'black'],
+  ['Mercedes Benz', 'C500', 'red'],
+  %w[BMW M5 red],
+  %w[BMW X6 white]
+])
 
+expected_flyweights = [
+  'Camaro2018_Chevrolet_pink',
+  'C300_Mercedes Benz_black',
+  'C500_Mercedes Benz_red',
+  'BMW_M5_red',
+  'BMW_X6_white'
+]
+assert_equal factory.list_flyweights, expected_flyweights
 
-# PENDING
+expected_operation = {
+  shared: ["BMW","M5","red"], unique: ["CL234IR","James Doe"]
+}
+assert_equal client_add_car_to_db(factory, 'CL234IR', 'James Doe', 'BMW', 'M5', 'red'), expected_operation
+puts('done.')
