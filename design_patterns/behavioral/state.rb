f0ae8b3f9bd9130eae504dd741b558ecfe78f9a7
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-11-12 10:36:25 -0600

    # state added...

--- design_patterns/behavioral/state.rb
+++ design_patterns/behavioral/state.rb
@@ -3,6 +3,9 @@
 # Problem: Allow an object to change behavior when its internal state changes
 # Solution: Extract state related behavior into separate state classes & object delegates state to instance of these state classes.
 
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
 class Context
   attr_accessor :state
   private :state
@@ -14,6 +17,7 @@ class Context
   def transition_to(state)
     @state = state
     @state.context = self
+    @state
   end
 
   def request1
@@ -37,5 +41,32 @@ class State
   end
 end
 
-# PENDING.
+class ConcreteStateA < State
+  def handle1
+    @context.transition_to(ConcreteStateB.new)
+  end
 
+  def handle2
+    'concrete state a:handle2'
+  end
+end
+
+class ConcreteStateB < State
+  def handle1
+    'concrete state b:handle1'
+  end
+
+  def handle2
+    @context.transition_to(ConcreteStateA.new)
+  end
+end
+
+def client()
+  Context.new(ConcreteStateA.new)
+end
+
+context = client()
+
+assert_equal context.request1.is_a?(ConcreteStateB), true
+assert_equal context.request2.is_a?(ConcreteStateA), true
+puts('done')
