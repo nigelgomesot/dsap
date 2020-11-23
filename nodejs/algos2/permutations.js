0e13d9bfa9e1baa5deb259b90b1fb399d48ccd1e
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-11-23 07:29:54 -0600

    # permutationWithoutRepetition added...

--- nodejs/algos2/permutations.js
+++ nodejs/algos2/permutations.js
@@ -5,7 +5,7 @@ const permutationWithRepetition = (array, r) => {
   const result = []
   const temp = []
 
-  backtrackPermutationWithRepetition(array, r, result, temp, 0)
+  backtrackPermutationWithRepetition(array, r, result, temp)
 
   return result
 }
@@ -39,3 +39,39 @@ expected = [
 assert.deepEqual(permutationWithRepetition(options, r), expected)
 console.log('permutationWithRepetition done.')
 
+const permutationWithoutRepetition = array => {
+  const result = []
+  const temp = []
+
+  backtrackPermutationWithoutRepetition(array, result, temp)
+
+  return result
+}
+
+const backtrackPermutationWithoutRepetition = (array, result, temp) => {
+  if (array.length === temp.length)
+    return result.push(temp.slice())
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+
+    if (temp.includes(element))
+      continue
+
+    temp.push(element)
+    backtrackPermutationWithoutRepetition(array, result, temp)
+    temp.pop()
+  }
+}
+
+options = ['A', 'B', 'C']
+expected = [
+  ['A', 'B', 'C'],
+  ['A', 'C', 'B'],
+  ['B', 'A', 'C'],
+  ['B', 'C', 'A'],
+  ['C', 'A', 'B'],
+  ['C', 'B', 'A'],
+]
+assert.deepEqual(permutationWithoutRepetition(options), expected)
+console.log('permutationWithoutRepetition done.')
