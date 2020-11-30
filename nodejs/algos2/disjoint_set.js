d50ca33ab5c2f96840fe04dc120948e4ebeec612
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-11-30 09:29:46 -0600

    # disjoint set union added...

--- nodejs/algos2/disjoint_set.js
+++ nodejs/algos2/disjoint_set.js
@@ -19,6 +19,14 @@ class DisjointSet {
     return this.find(this.parent[element])
   }
 
+  union(element1, element2) {
+    const parent1 = this.find(element1)
+    const parent2 = this.find(element2)
+
+    if (parent1 !== parent2)
+      this.parent[parent1] = parent2
+  }
+
   getSets(unverse) {
     const sets = []
 
@@ -37,3 +45,12 @@ ds = new DisjointSet()
 ds.makeSet(universe)
 
 assert.deepEqual(ds.getSets(universe), [1,2,3,4,5])
+
+ds.union(4,3)
+assert.deepEqual(ds.getSets(universe), [1,2,3,3,5])
+
+ds.union(2,1)
+assert.deepEqual(ds.getSets(universe), [1,1,3,3,5])
+
+ds.union(1,3)
+assert.deepEqual(ds.getSets(universe), [3,3,3,3,5])
