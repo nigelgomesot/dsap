a2a6a48b9a8e881bc78def1264081d355cebd385
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-12-26 12:50:07 -0600

    # priorityQueue WIP...

---
+++ nodejs/algos3/priorityQueue.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+
+class PriorityQueue {
+  constructor() {
+    this.list = []
+    this.length = 0
+  }
+
+  compare(item1, item2) {
+    return item1 > item2
+  }
+
+  enqueue(item) {
+    this.list[this.length] = item
+    this.length++
+
+    this.percolateUp(this.length - 1)
+  }
+
+  percolateUp(child) {
+    const parent = Math.floor((child - 1) / 2)
+
+    if (parent < 0)
+      return
+
+    if (this.compare(this.list[parent], this.list[child])) {
+      const temp = this.list[parent]
+      this.list[parent] = this.list[child]
+      this.list[child] = temp
+
+      this.percolateUp(parent)
+    }
+  }
+}
+
+const pq = new PriorityQueue()
+
+assert.deepEqual(pq.list, [])
+pq.enqueue(3)
+pq.enqueue(2)
+pq.enqueue(1)
+assert.deepEqual(pq.list, [1,3,2])
