6347878d034ade2360d7b3509aac26d2a0e9dc21
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-12 08:14:32 -0600

    # quickSelectIterative added...

--- nodejs/algos4/quick_select.js
+++ nodejs/algos4/quick_select.js
@@ -41,3 +41,51 @@ const partitionIndexRecursive = (array, left, right) => {
 position = 3
 assert.strictEqual(quickSelectRecursive(array, position), 6)
 console.log('quickSelectRecursive done.')
+
+const quickSelectIterative = (array, position) => {
+  return quickSelectIterativeUtil(array, position - 1)
+}
+
+const quickSelectIterativeUtil = (array, targetIndex) => {
+  let left = 0,
+      right = array.length - 1
+
+  while (left <= right) {
+    const partitionIndex = partitionIndexIterative(array, left, right)
+
+    if (partitionIndex === targetIndex)
+      return array[partitionIndex]
+    else if (partitionIndex < targetIndex)
+      left = partitionIndex + 1
+    else
+      right = partitionIndex - 1
+  }
+
+  return -1
+}
+
+const partitionIndexIterative = (array, left, right) => {
+  const pivot = array[right]
+  let partitionIndex = left
+
+  for (let i = left; i <= right; i++) {
+    if (array[i] < pivot) {
+      const temp = array[i]
+      array[i] = array[partitionIndex]
+      array[partitionIndex] = temp
+      partitionIndex++
+    }
+  }
+
+  const temp = array[right]
+  array[right] = array[partitionIndex]
+  array[partitionIndex] = temp
+
+  return partitionIndex
+}
+
+position = 3
+assert.strictEqual(quickSelectIterative(array, position), 6)
+position = 10
+assert.strictEqual(quickSelectIterative(array, position), -1)
+console.log('quickSelectIterative done.')
