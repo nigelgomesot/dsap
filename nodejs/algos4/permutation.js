174761c945ba288c00de96afa301f8fc4885fb16
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-16 12:22:15 -0600

    # permutationWithoutRepetition added...

--- nodejs/algos4/permutation.js
+++ nodejs/algos4/permutation.js
@@ -1,6 +1,9 @@
 const assert = require('assert')
 const { toNamespacedPath } = require('path')
 
+let options,
+    r
+
 const permutationWithRepetition = (array, r) => {
   const result = [],
         temp = []
@@ -41,3 +44,44 @@ expected = [
 ]
 assert.deepEqual(permutationWithRepetition(options, r), expected)
 console.log('permutationWithRepetition done.')
+
+const permutationWithoutRepetition = (array, r) => {
+  const result = [],
+        temp = []
+
+  backtrackPermutationWithoutRepetition(array, r, result, temp)
+
+  return result
+}
+
+const backtrackPermutationWithoutRepetition = (array, r, result, temp) => {
+  if (temp.length === r) {
+    result.push(temp.slice())
+
+    return
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+
+    if (temp.includes(element))
+      continue
+
+    temp.push(element)
+    backtrackPermutationWithoutRepetition(array, r, result, temp)
+    temp.pop()
+  }
+}
+
+options = ['A', 'B', 'C']
+r = 3
+expected = [
+  ['A', 'B', 'C'],
+  ['A', 'C', 'B'],
+  ['B', 'A', 'C'],
+  ['B', 'C', 'A'],
+  ['C', 'A', 'B'],
+  ['C', 'B', 'A'],
+]
+assert.deepEqual(permutationWithoutRepetition(options, r), expected)
+console.log('permutationWithoutRepetition done.')
