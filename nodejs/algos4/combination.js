bc0ac9c2ba7087afd03c2625d50cfd6e0f91f1c5
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-18 21:17:18 -0600

    # combinationWithRepetition added...

---
+++ nodejs/algos4/combination.js
@@ -0,0 +1,43 @@
+const assert = require('assert')
+
+let array,
+    r,
+    expected
+
+const combinationWithRepetition = (array, r) => {
+  const result = [],
+        temp = []
+
+  backtrackCombinationWithRepetition(array, r, result, temp, 0)
+
+  return result
+}
+
+const backtrackCombinationWithRepetition = (array, r, result, temp, startIndex) => {
+  if (temp.length === r) {
+    result.push(temp.slice())
+
+    return
+  }
+
+  for (let i = startIndex; i < array.length; i++) {
+    const element = array[i]
+
+    temp.push(element)
+    backtrackCombinationWithRepetition(array, r, result, temp, i)
+    temp.pop()
+  }
+}
+
+array = ['A', 'B', 'C']
+r = 2
+expected = [
+  ['A', 'A'],
+  ['A', 'B'],
+  ['A', 'C'],
+  ['B', 'B'],
+  ['B', 'C'],
+  ['C', 'C'],
+]
+assert.deepEqual(combinationWithRepetition(array, r), expected)
+console.log('combinationWithRepetition done.')
