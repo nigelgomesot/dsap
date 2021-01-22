3a8bb134008982ce0094bc1d6aa7806112a086fd
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-22 09:34:40 -0600

    # graph WIP...

---
+++ nodejs/algos4/graph.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+
+class Edge {
+  constructor(source, destination, cost) {
+    this.source = source
+    this.destination = destination
+    this.cost = cost
+  }
+}
+
+class Graph {
+  constructor(nodeCount, undirected = false) {
+    this.nodeCount = nodeCount
+    this.undirected = undirected
+
+    this.list = new Array(nodeCount).fill(null).map(() => new Array())
+  }
+
+  addEdge(source, destination, cost) {
+    const edge = new Edge(source, destination, cost)
+    const vertex = this.list[source]
+    vertex.push(edge)
+  }
+}
+
