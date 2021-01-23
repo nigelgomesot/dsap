bf903c14faac81ce97dea44b7e5798ae4f5c7c68
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-23 13:58:51 -0600

    # Graph added...

--- nodejs/algos4/graph.js
+++ nodejs/algos4/graph.js
@@ -21,5 +21,69 @@ class Graph {
     const vertex = this.list[source]
     vertex.push(edge)
   }
+
+  addEdges(items) {
+    for (const item of items) {
+      this.addEdge(
+        item.source,
+        item.destination,
+        item.cost
+      )
+
+      if (this.undirected) {
+        this.addEdge(
+          item.destination,
+          item.source,
+          item.cost
+        )
+      }
+    }
+  }
 }
 
+const items = [
+  {source: 0, destination: 1, cost: 100},
+  {source: 0, destination: 2, cost: 300},
+  {source: 1, destination: 2, cost: 200}
+]
+
+let graph
+let result = []
+let expectedResult = []
+
+graph = new Graph(3)
+graph.addEdges(items)
+graph.list.forEach((edges) => {
+  if (edges.length === 0)
+    return
+
+  for (const edge of edges)
+    result.push({source: edge.source, destination: edge.destination, cost: edge.cost})
+})
+expectedResult = items
+assert.deepEqual(result, expectedResult)
+
+result = []
+graph = new Graph(3, true)
+graph.addEdges(items)
+
+graph.list.forEach((edges) => {
+  if (edges.length === 0)
+    return
+
+  for (const edge of edges)
+    result.push({source: edge.source, destination: edge.destination, cost: edge.cost})
+})
+expectedResult = [
+  {source: 0, destination: 1, cost: 100},
+  {source: 0, destination: 2, cost: 300},
+  {source: 1, destination: 0, cost: 100},
+  {source: 1, destination: 2, cost: 200},
+  {source: 2, destination: 0, cost: 300},
+  {source: 2, destination: 1, cost: 200}
+]
+assert.deepEqual(result, expectedResult)
+
+console.log('Graph done')
+
+module.exports = { Graph, Edge }
