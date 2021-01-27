133fa74b538050ff220be68ec74a5415bdb05db0
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-27 08:25:02 -0600

    # cycleDetect1 added...

--- nodejs/algos4/cycle_detect1.js
+++ nodejs/algos4/cycle_detect1.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const { Graphh, Edge } = require('./graph')
+const { Graph, Edge } = require('./graph')
 
 const cycleDetect1 = graph => {
   const visited = new Array(graph.nodeCount).fill(false),
@@ -24,3 +24,45 @@ const cycleDetect1 = graph => {
   return false
 }
 
+const dfs = (graph, visited, departures, vertex, time) => {
+  visited[vertex] = true
+  time++
+
+  const edges = graph.list[vertex]
+
+  for (const edge of edges) {
+    if (!visited[edge.destination])
+      time = dfs(graph, visited, departures, edge.destination, time)
+  }
+
+  departures[vertex] = time
+  time++
+
+  return time
+}
+
+let nodeCount,
+    graph,
+    edges
+
+nodeCount = 7
+edges = [
+  {source: 0, destination: 1},
+  {source: 0, destination: 3},
+  {source: 1, destination: 2},
+  {source: 1, destination: 3},
+  {source: 3, destination: 2},
+  {source: 3, destination: 4},
+  {source: 5, destination: 6},
+  {source: 6, destination: 3},
+]
+
+graph = new Graph(7)
+graph.addEdges(edges)
+assert.equal(cycleDetect1(graph), false)
+
+edges.push({source: 3, destination: 0})
+graph = new Graph(7)
+graph.addEdges(edges)
+assert.equal(cycleDetect1(graph), true)
+console.log('cycleDetect1 done.')
