07535a2ae5040eb1554e573e154532a073256fdf
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-30 12:17:10 -0600

    # topologicalSort added...

--- nodejs/algos4/topological_sort.js
+++ nodejs/algos4/topological_sort.js
@@ -23,3 +23,42 @@ const topologicalSort = graph => {
 
   return result
 }
+
+const topologicalSortUtil = (graph, visited, departures, vertex, time) => {
+  visited[vertex] = true
+  time++
+
+  const edges = graph.list[vertex]
+
+  for (const edge of edges) {
+    if (!visited[edge.destination])
+      time = topologicalSortUtil(graph, visited, departures, edge.destination, time)
+  }
+
+  departures[vertex] = time
+  time++
+
+  return time
+}
+
+let nodeCount,
+    graph,
+    edges
+
+nodeCount = 8
+edges = [
+  {source: 0, destination: 6},
+  {source: 1, destination: 2},
+  {source: 1, destination: 4},
+  {source: 1, destination: 6},
+  {source: 3, destination: 0},
+  {source: 3, destination: 4},
+  {source: 5, destination: 1},
+  {source: 7, destination: 0},
+  {source: 7, destination: 1},
+]
+
+graph = new Graph(nodeCount)
+graph.addEdges(edges)
+assert.deepEqual(topologicalSort(graph),[7,5,3,1,4,2,0,6])
+console.log('topologicalSort done')
