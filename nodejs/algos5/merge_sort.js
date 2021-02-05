c54a93307bf4199ef549747447017daffd387145
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-02-05 08:57:07 -0600

    # mergeSort added...

--- nodejs/algos5/merge_sort.js
+++ nodejs/algos5/merge_sort.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-const { merge } = require('../../../jsnad/node-express-realworld-example-app/routes/api')
 
 const mergeSort = array => {
   const temp = []
@@ -20,3 +19,43 @@ const mergeSortUtil = (array, temp, leftStart, rightEnd) => {
 
   merge(array, temp, leftStart, mid, rightEnd)
 }
+
+const merge = (array, temp, leftStart, mid, rightEnd) => {
+  let left = leftStart,
+      leftEnd = mid,
+      right = mid + 1,
+      tempIndex = left
+
+  while (left <= leftEnd && right <= rightEnd) {
+    if (array[left] < array[right]) {
+      temp[tempIndex] = array[left]
+      left++
+    } else {
+      temp[tempIndex] = array[right]
+      right++
+    }
+    tempIndex++
+  }
+
+  while (left <= leftEnd) {
+    temp[tempIndex] = array[left]
+    left++
+    tempIndex++
+  }
+
+  while (right <= rightEnd) {
+    temp[tempIndex] = array[right]
+    right++
+    tempIndex++
+  }
+
+  for (let i = leftStart; i <= rightEnd; i++) {
+    array[i] = temp[i]
+  }
+}
+
+const array = [5,4,2,1,3]
+const expected = [1,2,3,4,5]
+
+assert.deepEqual(mergeSort(array), expected)
+console.log('done.')
