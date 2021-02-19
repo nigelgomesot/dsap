fac4803a46b2fb76f2967621a5d6aebebcaff139
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-02-19 09:58:39 -0600

    # DisjointSet WIP...

---
+++ nodejs/algos5/disjoint_set.js
@@ -0,0 +1,36 @@
+const assert = require('assert')
+
+class DisjointSet {
+  constructor() {
+    this.parent = {}
+  }
+
+  makeSet(elements) {
+    for (const element of elements) {
+      this.parent[element] = element
+    }
+  }
+
+  getSet(universe) {
+    const set = []
+
+    for (const element of universe) {
+      set.push(this.find(element))
+    }
+
+    return set
+  }
+
+  find(element) {
+    if (this.parent[element] === element)
+      return element
+
+    return this.find(this.parent[element])
+  }
+}
+
+const universe = [1,2,3,4,5]
+ds = new DisjointSet()
+ds.makeSet(universe)
+
+assert.deepEqual(ds.getSet(universe), [1,2,3,4,5])
