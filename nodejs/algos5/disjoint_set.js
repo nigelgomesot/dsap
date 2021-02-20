d3e23488ca20349a3e6d70eadd03bd803d55803a
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-02-20 13:22:21 -0600

    # DisjointSet added...

--- nodejs/algos5/disjoint_set.js
+++ nodejs/algos5/disjoint_set.js
@@ -27,6 +27,14 @@ class DisjointSet {
 
     return this.find(this.parent[element])
   }
+
+  union(element1, element2) {
+    const parent1 = this.find(element1),
+          parent2 = this.find(element2)
+
+    if (parent1 !== parent2)
+      this.parent[parent1] = parent2
+  }
 }
 
 const universe = [1,2,3,4,5]
@@ -34,3 +42,16 @@ ds = new DisjointSet()
 ds.makeSet(universe)
 
 assert.deepEqual(ds.getSet(universe), [1,2,3,4,5])
+
+ds.union(4,3)
+assert.deepEqual(ds.getSet(universe), [1,2,3,3,5])
+
+ds.union(2,1)
+assert.deepEqual(ds.getSet(universe), [1,1,3,3,5])
+
+ds.union(1,3)
+assert.deepEqual(ds.getSet(universe), [3,3,3,3,5])
+
+console.log('DisjointSet done')
+
+module.exports = DisjointSet
