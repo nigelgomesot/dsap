ac57e86db36a768fff91afe8f40c091f1dcdaefc
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-03-19 08:43:34 -0500

    # binarySearchRecursive added...

---
+++ nodejs/algos6/04_binary_search.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+
+const array = [1,2,3,4,5]
+
+const binarySearchRecursive = (array, target) => {
+  return  binarySearchRecursiveUtil(array, target, 0, array.length - 1)
+}
+
+const binarySearchRecursiveUtil = (array, target, left, right) => {
+  if (left > right)
+    return false
+
+  const mid = Math.floor((left + right)/ 2)
+
+  if (array[mid] === target)
+    return true
+  else if (array[mid] < target)
+    return binarySearchRecursiveUtil(array, target, mid + 1, right)
+  else
+    return binarySearchRecursiveUtil(array, target, left, mid - 1)
+}
+
+assert.strictEqual(binarySearchRecursive(array, 4), true)
+assert.strictEqual(binarySearchRecursive(array, 6), false)
+console.log('binarySearchRecursive done.')
+
+// PENDING: Complexity
+
