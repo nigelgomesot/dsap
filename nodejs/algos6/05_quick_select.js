83b25d62200a303fd215df11ae22f42cc6266615
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-03-23 09:59:54 -0500

    # quickSelectRecursive added...

---
+++ nodejs/algos6/05_quick_select.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+
+let array,
+    position
+
+const quickSelectRecursive = (array, position) => {
+  return quickSelectRecursiveUtil(array, position - 1, 0, array.length - 1) || -1
+}
+
+const quickSelectRecursiveUtil = (array, targetIndex, left, right) => {
+  const pivotIndex = partitionRecursive(array, left, right)
+
+  if (pivotIndex === targetIndex)
+    return array[pivotIndex]
+  else if (pivotIndex > targetIndex)
+    return quickSelectRecursiveUtil(array, targetIndex, left, pivotIndex - 1)
+  else
+    return quickSelectRecursiveUtil(array, targetIndex, pivotIndex + 1, right)
+}
+
+const partitionRecursive = (array, left, right) => {
+  const pivot = array[right]
+  let pivotIndex = left
+
+  for (let i = left; i <= right; i++) {
+    if (array[i] < pivot) {
+      [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]]
+      pivotIndex++
+    }
+  }
+
+  [array[right], array[pivotIndex]] = [array[pivotIndex], array[right]]
+
+  return pivotIndex
+}
+
+array = [10, 4, 5, 8, 6, 11, 26]
+position = 3
+assert.strictEqual(quickSelectRecursive(array, position), 6)
+position = 10
+assert.strictEqual(quickSelectRecursive(array, position), -1)
+console.log('quickSelectRecursive done.')
