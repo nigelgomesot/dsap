1eafb95a59309eb8249cd6d5297fd3cd22293e28
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-04-13 08:04:43 -0500

    # fetchBFS added...

--- nodejs/algos6/09_trees.js
+++ nodejs/algos6/09_trees.js
@@ -134,6 +134,27 @@ class Tree {
 
     return result
   }
+
+  fetchBFS() {
+    const result = [],
+          queue = []
+
+    queue.push(this.root)
+
+    while (queue.length) {
+      const node = queue.shift()
+
+      result.push(node.value)
+
+      if (node.left)
+        queue.push(node.left)
+
+      if (node.right)
+        queue.push(node.right)
+    }
+
+    return result
+  }
 }
 
 const values = [1,2,3,4,5]
@@ -175,5 +196,11 @@ tree = new Tree()
 tree.setBinaryLevelOrder(values)
 expectedResult = [1,2,4,5,3]
 assert.deepEqual(tree.fetchDFS(), expectedResult)
+
+// BFS
+tree = new Tree()
+tree.setBinaryLevelOrder(values)
+expectedResult = [1,2,3,4,5]
+assert.deepEqual(tree.fetchBFS(), expectedResult)
 
 console.log('Tree done.')
