fedea13944689d4923c2e6dcf7934ebd5d31bd1a
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-04-28 09:37:34 -0500

    # travelTime added...

--- nodejs/algos6/14_travel_times.js
+++ nodejs/algos6/14_travel_times.js
@@ -25,3 +25,47 @@ const travelTime = graph => {
   return result
 }
 
+const dfs = (graph, visited, departures, arrivals, vertex, time) => {
+  visited[vertex] = true
+  arrivals[vertex] = time
+  time++
+
+  const edges = graph.list[vertex]
+
+  for (const edge of edges) {
+    if (!visited[edge.destination])
+      time = dfs(graph, visited, departures, arrivals, edge.destination, time)
+  }
+
+  departures[vertex] = time
+  time++
+
+  return time
+}
+
+nodeCount = 8
+edges = [
+  {source: 0, destination: 1},
+  {source: 0, destination: 2},
+  {source: 2, destination: 3},
+  {source: 2, destination: 4},
+  {source: 3, destination: 1},
+  {source: 3, destination: 5},
+  {source: 4, destination: 5},
+  {source: 6, destination: 7},
+]
+expectedResult = [
+  {vertex: 0, arrival: 0, departure: 11},
+  {vertex: 1, arrival: 1, departure: 2},
+  {vertex: 2, arrival: 3, departure: 10},
+  {vertex: 3, arrival: 4, departure: 7},
+  {vertex: 4, arrival: 8, departure: 9},
+  {vertex: 5, arrival: 5, departure: 6},
+  {vertex: 6, arrival: 12, departure: 15},
+  {vertex: 7, arrival: 13, departure: 14},
+]
+
+graph = new Graph(nodeCount)
+graph.addEdges(edges)
+assert.deepEqual(travelTime(graph), expectedResult)
+console.log('travelTime done.')
