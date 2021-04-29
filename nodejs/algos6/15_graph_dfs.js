3bc858fee1dcb45295f7a1dafa49a189ef9cedd7
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-04-29 10:14:09 -0500

    # dfsRecursive added...

---
+++ nodejs/algos6/15_graph_dfs.js
@@ -0,0 +1,50 @@
+const assert = require('assert')
+const { Graph, Edge } = require('./10_graph')
+
+let nodeCount,
+    graph,
+    edges
+
+nodeCount = 13
+graph = new Graph(nodeCount, true)
+edges = [
+  {source: 1, destination: 2},
+  {source: 1, destination: 7},
+  {source: 1, destination: 8},
+  {source: 2, destination: 3},
+  {source: 2, destination: 6},
+  {source: 3, destination: 4},
+  {source: 3, destination: 5},
+  {source: 8, destination: 9},
+  {source: 8, destination: 12},
+  {source: 9, destination: 10},
+  {source: 9, destination: 11},
+]
+
+const dfsRecursive = graph => {
+  const visited = new Array(graph.nodeCount).fill(false),
+        result = []
+
+  for (let i = 0; i < graph.nodeCount; i++) {
+    if (!visited[i])
+      dfsRecursiveUtil(graph, visited, result, i)
+  }
+
+  return result
+}
+
+const dfsRecursiveUtil = (graph, visited, result, vertex) => {
+  visited[vertex] = true
+  result.push(vertex)
+
+  const edges = graph.list[vertex]
+
+  for (const edge of edges) {
+    if (!visited[edge.destination])
+    dfsRecursiveUtil(graph, visited, result, edge.destination)
+  }
+}
+
+graph.addEdges(edges)
+assert.deepEqual(dfsRecursive(graph), [0,1,2,3,4,5,6,7,8,9,10,11,12])
+console.log('dfsRecursive done.')
