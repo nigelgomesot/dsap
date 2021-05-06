c23e3e29637d62c07d9ee5a5fee608021d0d75e9
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-05-06 09:02:51 -0500

    # bfs added...

--- nodejs/algos6/16_graph_bfs.js
+++ nodejs/algos6/16_graph_bfs.js
@@ -14,3 +14,49 @@ const bfs = graph => {
   return result
 }
 
+const compareFn = (item1, item2) => {
+  return item1 > item2
+}
+
+const bfsUtil = (graph, visited, result, vertex) => {
+  const pq = new PriorityQueue(compareFn)
+
+  pq.enqueue(vertex)
+
+  while (!pq.isEmpty()) {
+    const dequeued = pq.dequeue()
+
+    visited[dequeued] = true
+    result.push(dequeued)
+
+    const edges = graph.list[dequeued]
+
+    for (const edge of edges) {
+      if (!visited[edge.destination])
+        pq.enqueue(edge.destination)
+    }
+  }
+}
+
+let nodeCount,
+    graph,
+    edges
+
+nodeCount = 13
+graph = new Graph(nodeCount, true)
+edges = [
+  {source: 1, destination: 2},
+  {source: 1, destination: 7},
+  {source: 1, destination: 8},
+  {source: 2, destination: 3},
+  {source: 2, destination: 6},
+  {source: 3, destination: 4},
+  {source: 3, destination: 5},
+  {source: 8, destination: 9},
+  {source: 8, destination: 12},
+  {source: 9, destination: 10},
+  {source: 9, destination: 11},
+]
+graph.addEdges(edges)
+assert.deepEqual(bfs(graph), [0,1,2,3,4,5,6,7,8,9,10,11,12])
+console.log('bfs done.')
