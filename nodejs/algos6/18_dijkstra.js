14d5941734cf4f755f50e2167542c4cb04ff393f
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-05-10 11:27:21 -0500

    # dijkstra added...

--- nodejs/algos6/18_dijkstra.js
+++ nodejs/algos6/18_dijkstra.js
@@ -33,7 +33,7 @@ const dijkstra = graph => {
 
       if (distance[edge.destination] > altDistance) {
         distance[edge.destination] = altDistance
-        previous[edge.destination] > edge.source
+        previous[edge.destination] = edge.source
 
         const nextNode = new Node(edge.destination, altDistance)
         pq.enqueue(nextNode)
@@ -41,5 +41,43 @@ const dijkstra = graph => {
     }
   }
 
-  // PENDING.
+  for (let i  = 0; i < graph.nodeCount; i++) {
+    result.push({
+      destination: i,
+      distance: distance[i],
+      previous: previous[i]
+    })
+  }
+
+  return result
 }
+
+let nodeCount,
+    graph,
+    edges,
+    expectedOutput
+
+nodeCount = 5
+graph = new Graph(nodeCount)
+edges = [
+  {source: 0, destination: 1, cost: 10},
+  {source: 0, destination: 4, cost: 3},
+  {source: 1, destination: 2, cost: 2},
+  {source: 1, destination: 4, cost: 4},
+  {source: 2, destination: 3, cost: 9},
+  {source: 3, destination: 2, cost: 7},
+  {source: 4, destination: 1, cost: 1},
+  {source: 4, destination: 2, cost: 8},
+  {source: 4, destination: 3, cost: 2},
+]
+graph.addEdges(edges)
+expectedOutput = [
+  {destination: 0, distance: 0, previous: -1},
+  {destination: 1, distance: 4, previous: 4},
+  {destination: 2, distance: 6, previous: 1},
+  {destination: 3, distance: 5, previous: 4},
+  {destination: 4, distance: 3, previous: 0}
+];
+
+assert.deepEqual(dijkstra(graph), expectedOutput)
+console.log('dijkstra done.')
