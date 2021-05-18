6613235c9916d0cde7d0a206a21ba68ee9a3fa21
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-05-18 12:31:25 -0500

    # binarySearchIterative added...

--- nodejs/algos7/04_binary_search.js
+++ nodejs/algos7/04_binary_search.js
@@ -23,3 +23,25 @@ const binarySearchRecursiveUtil = (array, target, left, right) => {
 assert.strictEqual(binarySearchRecursive(array, 4), true)
 assert.strictEqual(binarySearchRecursive(array, 6), false)
 console.log('binarySearchRecursive done.')
+
+const binarySearchIterative = (array, target) => {
+  let left = 0,
+      right = array.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (array[mid] === target)
+      return true
+    else if (array[mid] > target)
+      right = mid - 1
+    else
+      left = mid + 1
+  }
+
+  return false
+}
+
+assert.strictEqual(binarySearchIterative(array, 4), true)
+assert.strictEqual(binarySearchIterative(array, 6), false)
+console.log('binarySearchIterative done.')
