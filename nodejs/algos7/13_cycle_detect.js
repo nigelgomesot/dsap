aa0e091283a7298ca536818f7b0d19b59f75a083
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-06-22 07:10:54 -0500

    # cycleDetect2 added...

--- nodejs/algos7/13_cycle_detect.js
+++ nodejs/algos7/13_cycle_detect.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const { Graph, Edge } = require('./10_graph')
+const DisjointSet = require('./11_disjoint_set')
 
 let nodeCount,
     graph,
@@ -66,3 +67,55 @@ graph = new Graph(7)
 graph.addEdges(edges)
 assert.equal(cycleDetect1(graph), true)
 console.log('cycleDetect1 done.')
+
+const cycleDetect2 = graph => {
+  const ds = new DisjointSet(),
+        nodes = []
+
+  for (let i = 0; i < graph.nodeCount; i++)
+    nodes.push(i)
+
+  ds.makeSet(nodes)
+
+  for (let i = 0; i < graph.nodeCount; i++) {
+    const edges = graph.list[i]
+
+    for (const edge of edges) {
+      const sourceParent = ds.find(edge.source),
+            destinationParent = ds.find(edge.destination)
+
+      if (sourceParent === destinationParent)
+        return true
+      else
+        ds.union(sourceParent, destinationParent)
+    }
+  }
+
+  return false
+}
+
+edges = [
+  {source: 1, destination: 2},
+  {source: 1, destination: 7},
+  {source: 1, destination: 8},
+  {source: 2, destination: 3},
+  {source: 2, destination: 6},
+  {source: 3, destination: 4},
+  {source: 3, destination: 5},
+  {source: 8, destination: 9},
+  {source: 8, destination: 12},
+  {source: 9, destination: 10},
+  {source: 9, destination: 11},
+  {source: 11, destination: 12},
+];
+nodeCount = 13;
+graph = new Graph(nodeCount, false)
+graph.addEdges(edges)
+assert.equal(cycleDetect2(graph), true);
+
+edges.pop();
+graph = new Graph(nodeCount, false)
+graph.addEdges(edges)
+assert.equal(cycleDetect2(graph), false);
+
+console.log('cycleDetect2 done.')
