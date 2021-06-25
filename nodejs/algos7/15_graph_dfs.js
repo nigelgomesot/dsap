72158d6c998f22a3972465499315f2294cbfd54f
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-06-25 09:49:09 -0500

    # dfsIterative added...

--- nodejs/algos7/15_graph_dfs.js
+++ nodejs/algos7/15_graph_dfs.js
@@ -48,3 +48,40 @@ const dfsRecursiveUtil = (graph, visited, result, vertex) => {
 graph.addEdges(edges)
 assert.deepEqual(dfsRecursive(graph), [0,1,2,3,4,5,6,7,8,9,10,11,12])
 console.log('dfsRecursive done.')
+
+const dfsIterative = graph => {
+  const visited = new Array(graph.nodeCount).fill(false),
+        stack = [],
+        result = []
+
+  for (let i = 0; i < graph.nodeCount; i++) {
+    if (!visited[i]) {
+      stack.push(i)
+      dfsIterativeUtil(graph, visited, stack, result)
+    }
+  }
+
+  return result
+}
+
+const dfsIterativeUtil = (graph, visited, stack, result) => {
+  while (stack.length) {
+    const vertex = stack.pop()
+
+    if (!visited[vertex]) {
+      visited[vertex] = true
+      result.push(vertex)
+    }
+
+    const edges = graph.list[vertex]
+    edges.reverse()
+
+    for (const edge of edges) {
+      if (!visited[edge.destination])
+        stack.push(edge.destination)
+    }
+  }
+}
+
+assert.deepEqual(dfsIterative(graph), [0,1,2,3,4,5,6,7,8,9,10,11,12])
+console.log('dfsIterative done.')
